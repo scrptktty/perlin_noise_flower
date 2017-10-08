41273cc1d032750d036a9e43646ea070164388b4
Kachel Wittig <kjwittig@gmail.com>
2017-10-08 14:29:53 -0500

    Modified sketch doc

--- sketch.js
+++ sketch.js
@@ -5,8 +5,8 @@ var totalDegrees = 369
 var direction
 var rotation
 var r = 255
-var g = 128
-var b = 128
+var g = 255
+var b = 255
 
 function setup() {
     canvas = createCanvas(
@@ -16,63 +16,73 @@ function setup() {
     background(0)
     centerX = width / 2
     centerY = height / 2
-    radius = 1
+    //changes the size of the hole of the flower
+    radius = 30
     angleMode(DEGREES)
     translateX = 0
     translateY = 0
     opacity = 255
+    //changes the direction of all of the petals 'in' or 'out'
     direction = "out"
-    rotation = 24.0
+    //changed the roation variable from 24 to 400 and it centered the flower?
+    rotation = 400.0
 }
 
 function draw() {
 
     noFill()
     stroke(r, g, b, opacity)
+    // added stroke weight for wispy lines
+    strokeWeight(0.05)
     beginShape()
         for (let i=0; i <=totalDegrees; i++) {
-            var noiseFactor = noise(i / 40, frameCount / 320)
+            var noiseFactor = noise(i / 45, frameCount / 400)
             var x = centerX + radius * cos(i) * noiseFactor
             var y = centerY + radius * sin(i) * noiseFactor
             curveVertex(x, y)
-            rotate(PI/rotation);
+            rotate(-PI/rotation);
+
         }
     endShape(CLOSE)
     if (direction == "out") {
         if (radius > height / 2) {
             direction == "in"
         } else {
-            radius += 0.65
+            radius += 0.3
         }
     } else {
         if (radius <= 0) {
             radius = 100
             direction == "out"
         } else {
-            radius -= 0.65
+            radius -= 0.3
         }
     }
-    rotation += 0.1
-    if (frameCount > 200) {
+    rotation += .5
+    if (frameCount > 100) {
         translateX = 0
         translateY = 0
     }
-    if (r > 255) {
-        r = 0
-    }
-    if (g > 255) {
-        g = 0
-    }
-    if (b > 255) {
-        b = 0
-    }
+    // if (r > 255) {
+    //     r = 0
+    // }
+    // if (g > 255) {
+    //     g = 0
+    // }
+    // if (b > 255) {
+    //     b = 0
+    // }
     if (opacity == 0) {
         opacity = 255
     }
     translateX *= frameCount
     translateY *= frameCount
     opacity -= 1
-    r += 1
-    g += 1
-    b += 1
+
+    // one color flower
+    // r += 1
+    // g += 1
+    // b += 1
 }
+
+// how do I get it to stop looping?
